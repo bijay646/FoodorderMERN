47bf55a77f3b24707b023d610ca00d4a868c7fef
bijay646 <bijaylimbu1997@gmail.com>
2022-04-27 14:38:28 +0545

    client
    client for front end

---
+++ client/src/actions/pizzaActions.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export const getAllPizzas=()=>{
+
+    dispatch({type:'GET_PIZZAS_REQUEST'})
+    try{
+        const response = axios.get('/api/pizzas/getpizzas')
+        dispatch({type:'GET_PIZZAS_SUCCESS'})
+    }
+    catch(error){
+        dispatch({type:'GET_PIZZAS_FAILED'})
+
+    }
+
+}
---
+++ client/src/pizzasdata.js
@@ -0,0 +1,76 @@
+const pizzas = [{
+        "name": "PEPPER BARBECUE CHICKEN",
+        "varients": ["small", "medium", "large"],
+        "prices": [{
+            "small": 200,
+            "medium": 350,
+            "large": 400,
+        }],
+        "category": "nonveg",
+        "image": "https://img.freepik.com/free-photo/top-view-pepperoni-pizza-with-mushroom-sausages-bell-pepper-olive-corn-black-wooden_141793-2158.jpg?w=2000",
+        "description": "this pizza is really delicious"
+    },
+    {
+        "name": "PEPPER BARBECUE MUSHROOM",
+        "varients": ["small", "medium", "large"],
+        "prices": [{
+            "small": 250,
+            "medium": 300,
+            "large": 405,
+        }],
+        "category": "veg",
+        "image": "https://static.toiimg.com/thumb/56933159.cms?imgsize=686279&width=800&height=800",
+        "description": "this pizza is really good"
+    },
+    {
+        "name": "SMOKED PIZZA",
+        "varients": ["small", "medium", "large"],
+        "prices": [{
+            "small": 250,
+            "medium": 50,
+            "large": 450,
+        }],
+        "category": "nonveg",
+        "image": "https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395__480.jpg",
+        "description": "this pizza is big"
+    },
+    {
+        "name": "PEPPER BARBECUE SAUSAGE",
+        "varients": ["small", "medium", "large"],
+        "prices": [{
+            "small": 400,
+            "medium": 350,
+            "large": 400,
+        }],
+        "category": "nonveg",
+        "image": "https://www.recipetineats.com/wp-content/uploads/2020/05/Pepperoni-Pizza_5-SQjpg.jpg",
+        "description": "this pizza is really delicious"
+    },
+    {
+        "name": "GRILLED CHICKEN PIZZA",
+        "varients": ["small", "medium", "large"],
+        "prices": [{
+            "small": 500,
+            "medium": 350,
+            "large": 400,
+        }],
+        "category": "nonveg",
+        "image": "https://thumbs.dreamstime.com/z/pizza-salami-arugula-wooden-board-36924315.jpg",
+        "description": "this pizza is really delicious"
+    },
+    {
+        "name": "PEPPERONI PIZZA",
+        "varients": ["small", "medium", "large"],
+        "prices": [{
+            "small": 200,
+            "medium": 350,
+            "large": 400,
+        }],
+        "category": "nonveg",
+        "image": "https://www.simplyrecipes.com/thmb/8caxM88NgxZjz-T2aeRW3xjhzBg=/2000x1125/smart/filters:no_upscale()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__09__easy-pepperoni-pizza-lead-3-8f256746d649404baa36a44d271329bc.jpg",
+        "description": "The best pizza in the hotel"
+    }
+
+]
+
+export default pizzas;
